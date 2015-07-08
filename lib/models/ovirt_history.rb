f33e45745f5fdabbae7c140966a1bfe1d0fa2b85
Greg Blomquist <gblomqui@redhat.com>
2015-07-07 22:12:45 -0400

    Update activerecord dependency to 4.2.3
    ManageIQ has moved on to rails 4.2.3.  Bumping the version of activerecord in
    ovirt_metrics keeps this consistent with the ManageIQ application (the main
    consumer of this library).
    
    Awaiting other fallout of bumping the activerecord version.

--- lib/models/ovirt_history.rb
+++ lib/models/ovirt_history.rb
@@ -16,7 +16,7 @@ module OvirtMetrics
     end
 
     def self.with_time_range(start_time = nil, end_time = nil)
-      return scoped if start_time.nil?
+      return all if start_time.nil?
       where(:history_datetime => (start_time..(end_time || Time.now.utc)))
     end
 
--- ovirt_metrics.gemspec
+++ ovirt_metrics.gemspec
@@ -28,5 +28,5 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency "sqlite3"
   spec.add_development_dependency "coveralls"
 
-  spec.add_dependency "activerecord", "~> 3.2.0"
+  spec.add_dependency "activerecord", "~> 4.2.3"
 end
