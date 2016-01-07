eb3d8648929f6614179669bc116a75c118a8b646
Matthew Draper <matthew@trebex.net>
2016-01-07 12:41:43 +1030

    Use current ActiveRecord configuration settings
    The PK is detected automatically, and abstract_class + pluralize_table_names
    can take care of the rest of our needs -- so we can skip the inherited hook.

--- lib/models/ovirt_history.rb
+++ lib/models/ovirt_history.rb
@@ -2,18 +2,8 @@ require 'active_record'
 
 module OvirtMetrics
   class OvirtHistory < ActiveRecord::Base
-    # HACK: The following line is needed so that ActiveRecord 3.2.8 does not try
-    #   to dynamically define attribute methods for the OvirtHistory class
-    #   itself.  This class does not have a backing table, and is only needed to
-    #   allow a shared connection for all subclasses.  If not set, you get the
-    #   error "PGError: ERROR:  relation "rhevm_histories" does not exist"
-    @attribute_methods_generated = true
-
-    def self.inherited(other)
-      super
-      other.primary_key = :history_id
-      other.table_name  = other.name.split('::').last.underscore
-    end
+    self.abstract_class = true
+    self.pluralize_table_names = false
 
     def self.with_time_range(start_time = nil, end_time = nil)
       return all if start_time.nil?
